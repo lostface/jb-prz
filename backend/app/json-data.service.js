3f0da51e98d77b007cb344fe611ba74656632f5c
Zoltan Feher <feher.zoltan@fastmail.fm>
2016-08-08 15:16:09 +0200

    Implement json data service

---
+++ backend/app/json-data.service.js
@@ -0,0 +1,32 @@
+var jsonfile = require('jsonfile');
+
+module.exports = {
+  init: init,
+  getData: getData,
+};
+
+/**
+ *@param {String} file
+ */
+function init(file) {
+  this.file = file;
+  this.data = null;
+}
+
+/**
+ *@param {Function (err, data)} cb
+ */
+function getData(cb) {
+  if (this.data) {
+    return process.nextTick(() => cb(null, this.data));
+  }
+
+  jsonfile.readFile(this.file, jsonReadCb.bind(this));
+
+  function jsonReadCb(err, data) {
+    if (err) { return cb(err); }
+
+    this.data = data;
+    return cb(null, this.data);
+  }
+}
---
+++ backend/app/json-data.service.test.js
@@ -0,0 +1,123 @@
+var
+  test = require('tape'),
+  proxyquire =  require('proxyquire'),
+  sinon =  require('sinon'),
+  jsonFileStub = {};
+  JsonDataService = proxyquire('./json-data.service', { 'jsonfile': jsonFileStub });
+
+const DUMMY_FILE = 'dummy';
+const DUMMY_DATA = 'dummy';
+
+test('init initialization of file property', function(t) {
+  underTest = Object.create(JsonDataService);
+  underTest.init(DUMMY_FILE);
+
+  var
+    expected = DUMMY_FILE,
+    actual = underTest.file;
+
+  t.equal(actual, expected, `init() should set the file property to ${expected}`);
+
+  t.end();
+});
+
+test('init initialization of data property', function(t) {
+  underTest = Object.create(JsonDataService);
+  underTest.init(DUMMY_FILE);
+
+  var
+    expected = null,
+    actual = underTest.data;
+
+  t.equal(actual, expected, `init() should set the data property to ${expected}`);
+
+  t.end();
+});
+
+test('getData callback calling on first call', function(t) {
+  underTest = Object.create(JsonDataService);
+  underTest.init(DUMMY_FILE);
+
+  var readFileStub = jsonFileStub.readFile = sinon.stub();
+  readFileStub.callsArgWithAsync(1, null, DUMMY_DATA);
+
+  underTest.getData(cb);
+
+  delete jsonFileStub.readFile;
+
+  function cb(err, data) {
+    var
+      expected = DUMMY_DATA,
+      actual = data;
+
+    t.equal(actual, expected, 'getData() should call the callback with the data read from json');
+
+    t.end();
+  }
+});
+
+test('getData callback calling on second call', function(t) {
+  underTest = Object.create(JsonDataService);
+  underTest.init(DUMMY_FILE);
+  underTest.data = DUMMY_DATA;
+
+  jsonFileStub.readFile = sinon.stub();
+
+  underTest.getData(cb);
+
+  delete jsonFileStub.readFile;
+
+  function cb(err, data) {
+    var
+      expected = DUMMY_DATA,
+      actual = data;
+
+    t.equal(actual, expected, 'getData() should call the callback with cached data on second call');
+
+    t.end();
+  }
+});
+
+test('getData setting of data property on first call', function(t) {
+  underTest = Object.create(JsonDataService);
+  underTest.init(DUMMY_FILE);
+
+  var readFileStub = jsonFileStub.readFile = sinon.stub();
+  readFileStub.callsArgWithAsync(1, null, DUMMY_DATA);
+
+  underTest.getData(cb);
+
+  delete jsonFileStub.readFile;
+
+  function cb(err, data) {
+    var
+      expected = DUMMY_DATA,
+      actual = underTest.data;
+
+    t.equal(actual, expected, 'getData() should set the data property with the read json');
+
+    t.end();
+  }
+});
+
+test('getData json reading on second call', function(t) {
+  underTest = Object.create(JsonDataService);
+  underTest.init(DUMMY_FILE);
+  underTest.data = DUMMY_DATA;
+
+  var readFileSpy = jsonFileStub.readFile = sinon.spy();
+
+  underTest.getData(cb);
+
+  delete jsonFileStub.readFile;
+
+  function cb(err, data) {
+    var
+      expected = false,
+      actual = readFileSpy.called;
+
+    t.equal(actual, expected, 'getData() should not read the json again on second call');
+
+    t.end();
+  }
+});
