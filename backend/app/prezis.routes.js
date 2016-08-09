cd2dfbe1eee9231c1b7a923ec5ef024539c96f62
Zoltan Feher <feher.zoltan@fastmail.fm>
2016-08-09 12:54:36 +0200

    Implement prezis routes

--- backend/app/prezis.routes.js
+++ backend/app/prezis.routes.js
@@ -5,17 +5,35 @@ var
 module.exports = router;
 
 router
-  .get('/', listPrezis)
-  .get('/:preziId', findPrezi);
+  .get('/', findAll)
+  .get('/:preziId', findById);
 
-function listPrezis(req, res) {
-  // TODO implement
-  // TODO [req.query.search]
-  // TODO [req.query.orderBy]
-  res.send(req.query);
+function findAll(req, res) {
+  const
+    query = req.query,
+    search = query.search,
+    orderBy = query.orderBy,
+    descending = query.descending,
+    prezisService = req.app.locals.prezisService;
+
+  prezisService.findAll({ search: search, orderBy: orderBy, descending: descending }, findAllCb);
+
+  function findAllCb(err, data) {
+    // TODO error handling
+    res.json(data);
+  }
 }
 
-function findPrezi(req, res) {
-  // TODO implement
-  res.send(req.params)
+function findById(req, res) {
+  const
+    preziId = req.params.preziId,
+    prezisService = req.app.locals.prezisService;
+
+  prezisService.findById(preziId, findByIdCb);
+
+  function findByIdCb(err, data) {
+    // TODO error handling
+    // TODO prezi not exists case?
+    res.json(data);
+  }
 }
