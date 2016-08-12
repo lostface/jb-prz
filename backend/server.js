f3bfc2d8fba70bb916aab457d2f9d86a483fc9b5
Zoltan Feher <feher.zoltan@fastmail.fm>
2016-08-12 08:22:34 +0200

    Setup backend for Heroku

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,21 @@ const
   app = express(),
   prezisApp = require('./app/prezis.app');
 
+app.set('port', (process.env.PORT || 3000));
+
+app.use(function(req, res, next) {
+  // TODO refine origin when frontend deployed too
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res) {
+  res.send('Hello There');
+});
+
 app.use('/prezis', prezisApp);
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
