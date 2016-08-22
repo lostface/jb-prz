ed9bcb94c61c5448468cc81c16649d5ee40e72fc
Zoltan Feher <feher.zoltan@fastmail.fm>
2016-08-22 09:31:53 +0200

    Create new state only when real change occured in reducers

--- frontend/app/reducers.js
+++ frontend/app/reducers.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const {
-  RECEIVE_PREZIS,
-  SET_SEARCH_TEXT,
-  SET_SORT_BY,
-  SET_SORT_ASCENDING
-} = require('./actions');
+const
+  notDeepStrictEqual = require('not_deep_strict_equal'),
+  {
+    RECEIVE_PREZIS,
+    SET_SEARCH_TEXT,
+    SET_SORT_BY,
+    SET_SORT_ASCENDING
+  } = require('./actions');
 
 module.exports = reducer;
 
@@ -17,15 +19,38 @@ const defaultState = {
 };
 
 function reducer(state=defaultState, action) {
+  // TODO equality check can be generalized
   switch (action.type) {
-    case RECEIVE_PREZIS:
-      return { ...state, prezis: action.prezis };
-    case SET_SEARCH_TEXT:
-      return { ...state, searchText: action.searchText };
-    case SET_SORT_BY:
-      return { ...state, sortBy: action.sortBy };
-    case SET_SORT_ASCENDING:
-      return { ...state, sortAscending: action.sortAscending };
+    case RECEIVE_PREZIS: {
+      // TODO normalize
+      // TODO or check and use existing entities (creator) ...
+      const newValue = action.prezis;
+      return notDeepStrictEqual(newValue, state.prezis)
+        ? { ...state, prezis: newValue }
+        : state;
+    }
+
+    case SET_SEARCH_TEXT: {
+      const newValue = action.searchText;
+      return newValue !== state.searchText
+        ? { ...state, searchText: newValue }
+        : state;
+    }
+
+    case SET_SORT_BY: {
+      const newValue = action.sortBy;
+      return newValue !== state.sortBy
+        ? { ...state, sortBy: newValue }
+        : state;
+    }
+
+    case SET_SORT_ASCENDING: {
+      const newValue = action.sortAscending;
+      return newValue !== state.sortAscending
+        ? { ...state, sortAscending: newValue }
+        : state;
+    }
+
     default:
       return state;
   }
